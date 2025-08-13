477981cdffb9813926ab1b74215dc8819f52222d
Gabriel Santos <gabrielsantoscruz61@gmail.com>
2025-08-13 15:57:08 -0300

    ➕ deppendency(dependencias do shadcn)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 'use client'
 import { useEffect, useState } from "react";
 import { AlunoComStatus } from "./api/model/StatusAluno";
+import { ScrollArea } from "@/components/ui/scroll-area";
+import * as React from 'react';
 
 export default function Home() {
   const [alunos, setAlunos] = useState<AlunoComStatus[] | null>(null);
@@ -15,12 +17,15 @@ export default function Home() {
   }, []);
 
   return (
-    <div className="font-sans grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16">
-      {
-        alunos && alunos.map((aluno) => (
-          <div key={aluno.id}>{aluno.primeiro_nome} - {aluno.media}</div>
-        ))
-      }
+    <div className="font-sans grid grid-rows-[20px_1fr_20px] flex items-center justify-items-center min-h-screen p-8 pb-20 gap-160">
+      <ScrollArea className="h-[900px] w-[750px] rounded-md border p-4">
+        {alunos && alunos.map((aluno) => (
+          <React.Fragment key={aluno.id}>
+            <div className="text-sm">{aluno.primeiro_nome}</div>
+            <div className="text-sm">{aluno.media}</div>
+          </React.Fragment>
+        ))}
+      </ScrollArea>
     </div>
   );
 }
